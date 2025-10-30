bfe17b7d45979c77d4cbac8042f91f0e307eb92e
Gee <alickoamarko@gmail.com>
2025-10-30 18:02:28 +0100

    WIP

--- AppBundler.py
+++ AppBundler.py
@@ -1,5 +1,6 @@
 import sys
 from pathlib import Path
+from base64 import b64encode
 from tempfile import gettempdir
 
 
@@ -9,24 +10,28 @@ if len(sys.argv) < 3:
 
 
 target_file_data = ""
-with open(sys.argv[1], "r") as target:
-        target_file_data = target.read()
+with open(sys.argv[1], "rb") as target:
+        target_file_data = b64encode(target.read()).decode("ascii")
 
 target_file_name = Path(sys.argv[1]).stem
 
 with open("_TEMP.py", "w") as out:
         out.write(
                 "import os\n" +
+                "from base64 import b64decode\n" +
                 "from zipfile import ZipFile\n" +
-                "DATA = \"" + target_file_data + "\"\n" +
-                #"os.chdir(\"" + gettempdir() + "\")\n" +
-                "with open(\"TEMP.zip\", \"w\") as f:\n" +
-                "\tf.write(DATA)\n" +
-                "os.mkdir(\"" + target_file_name + "\")\n" +
-                "with ZipFile(\"TEMP.zip\", \"w\") as z:\n" +
+                "import subprocess\n" +
+                "DATA = '''" + target_file_data + "'''\n" +
+                #"os.chdir(\"" + gettempdir() + "\")\n" + # COMMENTED OUT FOR TESTING
+                "with open(\"TEMP.zip\", \"wb\") as f:\n" +
+                "\tf.write(b64decode(DATA))\n" +
+                "os.makedirs(\"" + target_file_name + "\", exist_ok=True)\n" +
+                "with ZipFile(\"TEMP.zip\", \"r\") as z:\n" +
                 "\tz.extractall(\"" + target_file_name + "\")\n" +
-                "os.system(\"" + " ".join(sys.argv[3:]) + "\")\n" +
+                "os.remove(\"TEMP.zip\")\n" +
+                "os.chdir(\"" + target_file_name + "\")\n" +
+                "subprocess.Popen(" + repr(sys.argv[2:]) + ", creationflags=0x8)\n" +
                 "exit()"
         )
 
-# TODO: Use Pyinstaller on it ^
+# TODO: Use Pyinstaller on _TEMP.py ^
