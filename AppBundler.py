dfea30478de6f67ee9cc47919144012423f48856
Gee <alickoamarko@gmail.com>
2026-01-13 14:07:44 +0100

    Fixed for Linux + optimized and refactored

--- AppBundler.py
+++ AppBundler.py
@@ -1,48 +1,54 @@
 import sys
 from pathlib import Path
 from base64 import b64encode
-from tempfile import gettempdir
 import PyInstaller.__main__
 import os
 import os.path
 import shutil
 
 
+RUN_PREFIX = "./" if (os.name != "nt") else ""
+
+
 if len(sys.argv) < 3:
         print("USAGE: AppBundler <TARGET>.zip <PATH/TO/APP/WITHIN> [ARGS...]")
         exit()
 
+sys.argv[2] = RUN_PREFIX + sys.argv[2]
 
 target_file_data = ""
 with open(sys.argv[1], "rb") as target:
         target_file_data = b64encode(target.read()).decode("ascii")
 
-temp_dir = "C:/ProgramData" if (os.name == "nt") else gettempdir()
-
 target_file_name = Path(sys.argv[1]).stem
 
 with open("OUT.py", "w") as out:
         out.write(
-                "import os\n" +
-                "from base64 import b64decode\n" +
-                "from zipfile import ZipFile\n" +
-                "import subprocess\n" +
-                "DATA = '''" + target_file_data + "'''\n" +
-                "os.chdir(\"" + temp_dir + "\")\n" +
-                "with open(\"TEMP.zip\", \"wb\") as f:\n" +
-                "\tf.write(b64decode(DATA))\n" +
-                "os.makedirs(\"" + target_file_name + "\", exist_ok=True)\n" +
-                "with ZipFile(\"TEMP.zip\", \"r\") as z:\n" +
-                "\tz.extractall(\"" + target_file_name + "\")\n" +
-                "os.remove(\"TEMP.zip\")\n" +
-                "os.chdir(\"" + target_file_name + "\")\n" +
-                "subprocess.Popen(" + repr(sys.argv[2:]) + ", creationflags= 0x8 if os.name == \"nt\" else 0x0, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)\n" +
-                "sys.exit()"
+                "from tempfile import gettempdir\n"
+                "import os\n"
+                "from base64 import b64decode\n"
+                "from zipfile import ZipFile\n"
+                "import subprocess\n"
+                "import sys\n"
+                "DATA = '''" + target_file_data + "'''\n"
+                "os.chdir(gettempdir())\n"
+                "with open('TEMP.zip', 'wb') as f:\n"
+                "\tf.write(b64decode(DATA))\n"
+                "os.makedirs('" + target_file_name + "', exist_ok=True)\n"
+                "with ZipFile('TEMP.zip', 'r') as z:\n"
+                "\tz.extractall('" + target_file_name + "')\n"
+                "os.remove('TEMP.zip')\n"
+                "os.chdir('" + target_file_name + "')\n"
+                "if os.name != 'nt': os.chmod('" + sys.argv[2] + "', 0o777)\n"
+                "subprocess.Popen(" + repr(sys.argv[2:]) + ")\n"
+                "sys.exit()\n"
         )
 
 PyInstaller.__main__.run([
         "OUT.py",
         "--onefile",
+        "--windowed",
+        "--noupx",
         "--distpath", ".",
         "--clean"
 ])
